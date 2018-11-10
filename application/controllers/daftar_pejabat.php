2810b8ab8381e94a0aa1484b39080ed6338fab9d
irpandamanik <33111318+irpandamanik@users.noreply.github.com>
2018-11-10 16:10:00 +0700

    Add files via upload

---
+++ application/controllers/daftar_pejabat.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class daftar_pejabat extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('daftar_pejabat');
+	}
+}
---
+++ application/controllers/rpjmd_kabupaten.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class rpjmd_kabupaten extends CI_Controller {
+	public function index()
+	{
+		$this->load->view('rpjmd_kabupaten');
+	}
+}
